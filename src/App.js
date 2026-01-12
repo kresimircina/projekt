ccd53c7c952fa527474224a3630b7bba8db382ff
kresimircina <kresimircina@gmail.com>
2026-01-12 19:57:14 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import Test from "./components/Test";
+import Test2 from "./components/Test2";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <class Name="icon"></class>
+     <Test />
+      <h2>Naslov 2</h2>
+    </>
   );
 }
 
---
+++ src/components/Test.js
@@ -0,0 +1,9 @@
+import "./Test.css"
+
+const Test = () => {
+    return (
+        <h1>Testni naslov iz komponente</h1>
+    )
+}
+
+export default Test;
